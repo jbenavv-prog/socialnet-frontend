dbada9c28e3840dd7e72d1ad3ca09dc4bb2016e9
julian camilo benavides vanegas <correoj94@gmail.com>
2022-11-13 07:18:34 -0500

    firt commit

---
+++ src/app/profile/profile-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProfileRoutingModule {}
