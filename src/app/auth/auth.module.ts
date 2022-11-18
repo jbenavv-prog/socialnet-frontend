b618efcd480692ce766b931bfec04981e697edff
julian camilo benavides vanegas <correoj94@gmail.com>
2022-11-18 18:35:26 -0500

    basic form signup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,17 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    SignupComponent
-  ],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    MatInputModule
-  ]
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  form = this.fb.group({
+    fullName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+ if (this.form.valid) {
+  console.log('Formulario Valido');
+  console.log(this.form.value);
+ }
+ else {
+  console.log('Formulario Inválido');
+
+ }
   }
-
 }
